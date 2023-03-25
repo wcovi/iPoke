e15570008b9b28d725493e720a894b2afb64222b
Wolfgang F. Covi <wcovi@yahoo.com.br>
2023-03-25 11:41:04 -0300

    correcao de componentes e constrains na tela login e navegacao para a tela de forgotpassword

--- iPoke/iPoke/Login/ViewController.swift
+++ iPoke/iPoke/Login/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func forgotPasswordButton(_ sender: Any) {
+        let vc = ForgotPasswordViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    
     @IBAction func loginButton(_ sender: Any) {
         let vc = TabBarController()
         navigationController?.pushViewController(vc, animated: true)
