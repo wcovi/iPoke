0c1b396071d15857e390f0059ef2aae86a99f41a
Vitor Ernane <vitorernane@hotmail.com>
2023-03-28 15:05:19 -0300

    Foi feito alguns testes para tentar chamar a tela quando clicado no botão Forgot Password abrir a tela correta e criados os IBOutlets para fazer as validações

--- iPoke/iPoke/ForgotPassword/ForgotPasswordViewController.swift
+++ iPoke/iPoke/ForgotPassword/ForgotPasswordViewController.swift
@@ -8,23 +8,30 @@
 import UIKit
 
 class ForgotPasswordViewController: UIViewController {
-
+    
+    @IBOutlet weak var viewBackgroundImage: UIImageView!
+    @IBOutlet weak var logoImage: UIImageView!
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordAgainLabel: UILabel!
+    @IBOutlet weak var passwordAgainTextField: UITextField!
+    
+    @IBOutlet weak var refreshPasswordButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+    
+        
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configBackgroundImage() {
+//        viewBackgroundImage.image = UIImage(named: "Dark Side")
     }
-    */
 
 }
