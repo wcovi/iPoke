f4056be76bb8476d7465533b62adef10062a5298
Wolfgang F. Covi <wcovi@yahoo.com.br>
2023-04-22 22:19:36 -0300

    Organização da tela de Login e ForgotPassword segundo a arquitetura MVVM.

---
+++ iPoke/iPoke/ForgotPassword/ViewModel/ForgotPassword.swift
@@ -0,0 +1,12 @@
+//
+//  ForgotPassword.swift
+//  iPoke
+//
+//  Created by Wolfgang Florian Covi on 22/04/23.
+//
+
+import Foundation
+
+class ForgotPassword {
+    
+}
---
+++ iPoke/iPoke/Login/ViewModel/Login.swift
@@ -0,0 +1,12 @@
+//
+//  Login.swift
+//  iPoke
+//
+//  Created by Wolfgang Florian Covi on 22/04/23.
+//
+
+import Foundation
+
+class Login {
+
+}
